d23cea8590e08aa61df3d40554b09da8a86812eb
Steven Zilberberg <srz222@gmail.com>
2017-08-16 18:06:43 -0400

    Added example 5

---
+++ example5/ex5.py
@@ -0,0 +1,13 @@
+name = 'Zed A. Shaw'
+age = 3
+height = 75
+weight = 180
+eyes = 'Blue'
+hair = 'Brown'
+
+print "Lets talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d lbs." % weight
+print "He's got %s eyes and %s hair" % (eyes, hair)
+
+print "If I add %d, %d, and %d I would get %d." % (age, height, weight, age + height + weight)
